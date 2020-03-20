1daebb5403c1a8372e89656756b29a123846319a
Marco Karaula <marco.karaula@flixbus.com>
2020-03-20 23:49:14 +0100

    Testing prepush

--- source/app.tsx
+++ source/app.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-export const App = () => (
+export const App: React.FC = () => (
   <Router>
     <Route path="/">
       <p>Supermarket Frontend</p>
     </Route>
   </Router>
 )
-
-const test = 'aaa'
-const test2 = 'bbbb'
