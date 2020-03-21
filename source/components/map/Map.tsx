77999f6d8f9d8d37de43c7549726f96dc76eb724
Matteo Maronati <me@mattinthe.net>
2020-03-21 02:39:06 +0100

    Update maps key

--- source/components/map/Map.tsx
+++ source/components/map/Map.tsx
@@ -4,7 +4,9 @@ import GoogleMapReact from 'google-map-react'
 export const Map: FunctionComponent = () => (
   <div style={{ height: '100vh', width: '100%' }}>
     <GoogleMapReact
-      bootstrapURLKeys={{ key: 'AIzaSyDS9TsYS9mJMEJxwsmCow28CLxHGnFwrVA' }}
+      bootstrapURLKeys={{
+        key: 'AIzaSyCsqZRajAsJUsoiW15aSyzT0fDcAsldc2w'
+      }}
       defaultCenter={{
         lat: 59.95,
         lng: 30.33
